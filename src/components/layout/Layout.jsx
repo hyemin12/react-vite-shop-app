1d7c23bce35ef1c086f47c814b18b37afac06e81
hyemin12 <khnm31@naver.com>
2023-11-04 11:09:21 +0900

    style: global style 설정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div>
+      Layout
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;
