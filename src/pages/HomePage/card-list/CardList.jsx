fe3094c8298b6459c20818f50487be0e6c8fa7e6
hyemin12 <khnm31@naver.com>
2023-11-04 20:23:03 +0900

    feat: 로딩 시 보여줄 스텔레톤 UI 생성

--- src/pages/HomePage/card-list/CardList.jsx
+++ src/pages/HomePage/card-list/CardList.jsx
@@ -3,16 +3,22 @@ import { fetchProducts } from "../../../store/products/products.slice";
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
 import CardItem from "../card-item/CardItem";
 import styles from "./CardList.module.scss";
+import CardSkeleton from "../card-skeleton/CardSkeleton";
 
 const CardList = () => {
   const dispatch = useAppDispatch();
 
-  const { products } = useAppSelector((state) => state.productsSlice);
+  const { isLoading, products } = useAppSelector(
+    (state) => state.productsSlice
+  );
   const category = useAppSelector((state) => state.categoriesSlice);
 
   useEffect(() => {
     dispatch(fetchProducts(category?.toLowerCase()));
   }, [category]);
+
+  if (isLoading) return <CardSkeleton />;
+
   return (
     <div className={styles.card_list}>
       {products &&
--- src/pages/HomePage/card-skeleton/CardSkeleton.jsx
+++ src/pages/HomePage/card-skeleton/CardSkeleton.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./CardSkeleton.module.scss";
+import "react-loading-skeleton/dist/skeleton.css";
+import Skeleton from "react-loading-skeleton";
 
 const CardSkeleton = () => {
-  return <div>CardSkeleton</div>;
+  return (
+    <div className={styles.card_skeleton_container}>
+      {Array(4)
+        .fill()
+        .map((_, idx) => (
+          <div className={styles.card_skeleton} key={idx}>
+            <Skeleton height={350} />
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default CardSkeleton;
