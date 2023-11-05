d9542f3f95b01b2f6b070bc25030c21a1096d4e9
hyemin12 <khnm31@naver.com>
2023-11-05 19:10:48 +0900

    build: vite가 절대경로 인식못하는 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+      { find: "@utils", replacement: path.resolve(__dirname, "src/utils") },
+      { find: "@store", replacement: path.resolve(__dirname, "src/store") },
+      { find: "@hooks", replacement: path.resolve(__dirname, "src/hooks") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+    ],
+  },
+});
