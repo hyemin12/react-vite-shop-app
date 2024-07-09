f7f9e2795a09530c4026c3e20bb43c8ecdbb60c5
hyemin12 <khnm31@naver.com>
2024-07-09 18:41:13 +0900

    refactor: 장바구니 페이지에서 로그인할 경우 장바구니 페이지로 이동하기

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
@@ -12,9 +12,11 @@ import { setUserId } from "src/store/cart/cart.slice";
 import { firebaseErrorHandler } from "src/utils/firebaseErrorHandler";
 
 const LoginPage = () => {
+  const { pathname } = useLocation();
   const [firebaseError, setFirebaseError] = useState<string | null>(null);
   const dispatch = useAppDispatch();
-  const { moveToMainPageHandler } = useNavigationHandlers();
+  const { moveToMainPageHandler, moveToCartPageHandler } =
+    useNavigationHandlers();
 
   const logInHandler = async (email: string, password: string) => {
     try {
@@ -27,6 +29,11 @@ const LoginPage = () => {
         })
       );
       dispatch(setUserId(user.uid));
+
+      if (pathname === "cart") {
+        moveToCartPageHandler();
+        return;
+      }
       moveToMainPageHandler();
     } catch (error) {
       if (error instanceof FirebaseError) {
