ec39d0cbed8b56679aaa35bac88cd49d2c9320b9
hyemin12 <khnm31@naver.com>
2024-07-09 18:11:29 +0900

    refactor: 회원가입 페이지 코드 합치기

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,12 +1,55 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import SignUp from "./sign-up/SignUp";
+import { FirebaseError } from "firebase/app";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "src/firebase";
+import { useAppDispatch } from "src/hooks/redux";
+import useNavigationHandlers from "src/hooks/useNavigationHandlers";
+import Form from "src/components/form/Form";
+import { setUser } from "src/store/user/user.slice";
+import { setUserId } from "src/store/cart/cart.slice";
+import { firebaseErrorHandler } from "src/utils/firebaseErrorHandler";
 
 const RegisterPage = () => {
+  const [firebaseError, setFirebaseError] = useState<string | null>(null);
+  const dispatch = useAppDispatch();
+  const { moveToMainPageHandler } = useNavigationHandlers();
+
+  const signUpAndLoginHandler = async (email: string, password: string) => {
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      dispatch(
+        setUser({
+          email: user.email,
+          token: user.refreshToken,
+          id: user.uid,
+        })
+      );
+      dispatch(setUserId(user.uid));
+      moveToMainPageHandler();
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        setFirebaseError(firebaseErrorHandler(error.code));
+        return;
+      }
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="page">
-      <div className="form_container">
+    <div className='page'>
+      <div className='form_container'>
         <h1>회원가입</h1>
-        <SignUp />
+        <Form
+          title='가입하기'
+          getDataForm={signUpAndLoginHandler}
+          firebaseError={firebaseError}
+        />
         <p>
           이미 계정이 있습니까? <Link to={"/login"}>로그인</Link>
         </p>
