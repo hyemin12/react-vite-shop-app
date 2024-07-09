fa0e57d0308aeb7663de3f06fb4015a225bb3c55
hyemin12 <khnm31@naver.com>
2024-07-09 18:11:18 +0900

    refactor: 로그인페이지 코드 합치기

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import SignIn from "./sigin-in/SignIn";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "src/firebase";
+import { useAppDispatch } from "src/hooks/redux";
+import useNavigationHandlers from "src/hooks/useNavigationHandlers";
+import Form from "src/components/form/Form";
+import { setUser } from "src/store/user/user.slice";
+import { setUserId } from "src/store/cart/cart.slice";
+import { firebaseErrorHandler } from "src/utils/firebaseErrorHandler";
 
 const LoginPage = () => {
+  const [firebaseError, setFirebaseError] = useState<string | null>(null);
+  const dispatch = useAppDispatch();
+  const { moveToMainPageHandler } = useNavigationHandlers();
+
+  const logInHandler = async (email: string, password: string) => {
+    try {
+      const { user } = await signInWithEmailAndPassword(auth, email, password);
+      dispatch(
+        setUser({
+          email: user.email,
+          token: user.refreshToken,
+          id: user.uid,
+        })
+      );
+      dispatch(setUserId(user.uid));
+      moveToMainPageHandler();
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        setFirebaseError(firebaseErrorHandler(error.code));
+        return;
+      }
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="page">
-      <div className="form_container">
+    <div className='page'>
+      <div className='form_container'>
         <h1>로그인</h1>
-        <SignIn />
+        <Form
+          title='로그인'
+          firebaseError={firebaseError}
+          getDataForm={logInHandler}
+        />
         <p>
-          계정이 없습니까?{" "}
-          <Link className="text-primary" to={"/register"}>
+          계정이 없습니까?
+          <Link className='text-primary' to={"/register"}>
             가입하기
           </Link>
         </p>
