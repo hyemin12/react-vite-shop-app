643cd57f9c58d02572994a6490e582a31cc3a14e
hyemin12 <khnm31@naver.com>
2024-07-10 15:55:12 +0900

    refactor: firebase 환경변수 코드 리팩토링

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,40 +4,22 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const apiKey =
-  import.meta.env.VITE_FIREBASE_API_KEY ?? process.env.VITE_FIREBASE_API_KEY;
-const authDomain =
-  import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ??
-  process.env.VITE_FIREBASE_AUTH_DOMAIN;
-const projectId =
-  import.meta.env.VITE_FIREBASE_PROJECT_ID ??
-  process.env.VITE_FIREBASE_PROJECT_ID;
-const storageBucket =
-  import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ??
-  process.env.VITE_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId =
-  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ??
-  process.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
-const appId =
-  import.meta.env.VITE_FIREBASE_APP_ID ?? process.env.VITE_FIREBASE_APP_ID;
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey,
-  authDomain,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 console.log(
-  apiKey,
-  authDomain,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId
+  firebaseConfig.apiKey,
+  firebaseConfig.authDomain,
+  firebaseConfig.projectId,
+  firebaseConfig.storageBucket,
+  firebaseConfig.messagingSenderId,
+  firebaseConfig.appId
 );
 
 // Initialize Firebase
